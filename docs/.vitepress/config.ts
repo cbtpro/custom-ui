c7ae6fd9d8bbfd179d2b3018150e138940808085
chenbitao <cbtpro@163.com>
2023-07-13 01:07:16 +0800

    ci: 修改vitepress的base目录，可以部署在任何环境下

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'My Awesome Project',
   description: 'A VitePress Site',
+  base: './',
   srcDir: './src',
   outDir: '../public',
   themeConfig: {
